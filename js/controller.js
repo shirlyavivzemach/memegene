58e07fe8343c066a0d046bbcd1e96fdcf21f0bb3
Shirly <shirly1986@gmail.com>
2020-08-15 22:00:02 +0300

    version 1

--- js/controller.js
+++ js/controller.js
@@ -2,76 +2,117 @@
 
 
 function onInit() {
-    gCanvas = document.getElementById('myCanvas');
-    gCtx = gCanvas.getContext('2d');
+
     // window.addEventListener('resize', function(){
-    // //     resizeCanvas()
+
+    //     resizeCanvas()
     // })
 
-    // drawImgFromlocal()
+    gCanvas = document.getElementById('myCanvas');
+    gCtx = gCanvas.getContext('2d');
 
     renderGallery()
 }
 
-function renderGallery(){
-    let elGallery=document.querySelector('.gallery')
-        for(var i=0;i<gImgs.length;i++){
-            elGallery.innerHTML+=`<img onclick="onClickImg(${gImgs[i].id})" src=${gImgs[i].url} class="gallery-img"/>`
-        }  
+function renderGallery() {
+    let elGallery = document.querySelector('.gallery')
+    elGallery.innerHTML=''
+    for (var i = 0; i < filteredImgs.length; i++) {
+        elGallery.innerHTML += `<img onclick="onClickImg(${filteredImgs[i].id})" src=${filteredImgs[i].url} class="gallery-img"/>`
     }
-
-function onClickImg(id){
-    setSelectedImage(id)
-    let elModal=document.querySelector('.open-img')
-    elModal.style.display='unset'
-    document.querySelector('.gallery').hidden=true
-    document.querySelector('.About').hidden=true
-
-    console.log(elModal.style.display)   
+    renderCategories()
 }
+
+function onClickImg(id) {
+    setSelectedImage(id)
+    let elSelectedImg = document.querySelector('.open-img')
+    elSelectedImg.style.display = 'block'
+    document.querySelector('.gallery').style.display = 'none'
+    document.querySelector('.About').hidden = true
+    document.querySelector('.name-icon-wrapper').style.display = 'none'
+    document.querySelector('.categories-container').hidden = true
+}
+
+
 
 function onCloseImg() {
-let elModal=document.querySelector('.open-img')
-document.querySelector('.gallery').hidden=false
-    elModal.style.display='none'
-  
+    let elSelectedImg = document.querySelector('.open-img')
+    document.querySelector('.gallery').style.display = 'inline-grid'
+    elSelectedImg.style.display = 'none'
+    document.querySelector('.About').hidden = false
+    document.querySelector('input').hidden = false
+    document.querySelector('.name-icon-wrapper').style.display = 'flex'
+ document.querySelector('.categories-container').hidden = false
 }
 
 
-function onDcreaseFont(){
-gMeme.lines[gMeme.selectedLineIdx].size--;
-drawImgFromlocal(gMeme.selectedImgId)
+function onDcreaseFont() {
+    gMeme.lines[gMeme.selectedLineIdx].size--;
+    drawImgFromlocal(gMeme.selectedImgId)
 
 }
 
-function onIncreaseFont(){
+function onIncreaseFont() {
     gMeme.lines[gMeme.selectedLineIdx].size++
     drawImgFromlocal(gMeme.selectedImgId)
-    }
+}
 
-function onSwitchLine(){
-    let selectedLine= gMeme.selectedLineIdx
+function onSwitchLine() {
+    let selectedLine = gMeme.selectedLineIdx
     let saveSelectedText = null
-    if(selectedLine===gMeme.lines.length-1 && selectedLine > 0){
-        gMeme.selectedLineIdx=0
+    if (selectedLine === gMeme.lines.length - 1 && selectedLine > 0) {
+        gMeme.selectedLineIdx = 0
     } else {
         gMeme.selectedLineIdx++
     }
-   saveSelectedText=gMeme.lines[selectedLine].txt;
-    
+    saveSelectedText = gMeme.lines[selectedLine].txt;
+    drawImgFromlocal(gMeme.selectedImgId)
 }
 
-function onDeleteText(){
+function onDeleteText() {
     gMeme.lines[gMeme.selectedLineIdx].txt = ''
-   drawImgFromlocal(gMeme.selectedImgId)
+    drawImgFromlocal(gMeme.selectedImgId)
+}
+
+
+function onChangeLineHeight(isIncrease) {
+    if (isIncrease) {
+        gMeme.lines[gMeme.selectedLineIdx].y++
+    } else {
+        gMeme.lines[gMeme.selectedLineIdx].y--
+    }
+    drawImgFromlocal(gMeme.selectedImgId)
+}
+
+    function onColorSelect(color_name) {
+        gMeme.lines[gMeme.selectedLineIdx].color = color_name;
+        
+    
     }
 
 
-    function onChangeLineHeight(isIncrease){
-        if(isIncrease){
-            gMeme.lines[gMeme.selectedLineIdx].y++
-        }else{
-            gMeme.lines[gMeme.selectedLineIdx].y--
-        }
-        drawImgFromlocal(gMeme.selectedImgId)
+function onChangeLineAlign(alignStr) {
+    let align = gCanvas.width / 2
+    console.log(align);
+    switch (alignStr) {
+        case 'right':
+            align = gCanvas.width - 100
+            break;
+        case 'left':
+            align = 100
+
+            break;
+        case 'center':
+            align = gCanvas.width / 2
+            break;
+        default:
+            break;
     }
+
+    gMeme.lines[gMeme.selectedLineIdx].x = align
+    drawImgFromlocal(gMeme.selectedImgId)
+}
+
+function onToggleMenu() {
+    document.body.classList.toggle('menu-open');
+}
--- js/service.js
+++ js/service.js
@@ -25,7 +25,9 @@ var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny'] },
 {id: 18, url: 'img/18.jpg', keywords: ['comics']},
 
 ];
-
+let filteredImgs = gImgs
+let canvasCenter = 300
+let gMeasureText = {width:0,height:10, x:canvasCenter,y:30}
 var gMeme = {
     selectedImgId: 1,
     selectedLineIdx: 0,
@@ -34,9 +36,9 @@ var gMeme = {
             txt: '',
             size: 20,
             align: 'left',
-            color: 'red',
+            color: 'white',
             y: 0,
-            x: 0
+            x:canvasCenter
         }
     ]
 }
@@ -62,21 +64,38 @@ function drawImgFromlocal(imageUrl) {
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
         for(let i = 0 ; i < gMeme.lines.length;i++){
-            console.log(i,gMeme.lines[i].y)
-            drawText(gMeme.lines[i].txt, 225, gMeme.lines[i].y)
+            if(gMeme.selectedLineIdx === i){
+                drawTextHighLight(gMeme.lines[i].txt,gMeme.lines[i].x, gMeme.lines[i].y)
+            }
+           
+            drawText(gMeme.lines[i].txt, gMeme.lines[i].x, gMeme.lines[i].y)
         }
     } 
 }
 
+function drawTextHighLight(text,x,y){
+    let measure = gCtx.measureText(text)
+    gMeasureText.width= text==='' ? 0 :measure.width +40;
+    gMeasureText.height= gMeme.lines[gMeme.selectedLineIdx].size*2
+    gMeasureText.x= x -measure.actualBoundingBoxLeft-20
+    gMeasureText.y= y === 0 ? 20:gMeme.lines[gMeme.selectedLineIdx].y -30
+
+    gCtx.fillRect(gMeasureText.x,gMeasureText.y,gMeasureText.width,gMeasureText.height)
+    gCtx.fillStyle= 'rgb(255,255,255)'
+    gCtx.globalAlpha = 1;
+   
+}
 
 function drawText(text, x, y) {
    gCtx.lineWidth = '2';
     gCtx.strokeStyle = 'black';
-    gCtx.fillStyle = 'white';
+    gCtx.fillStyle = `${gMeme.lines[gMeme.selectedLineIdx].color}`
     gCtx.font = `${gMeme.lines[gMeme.selectedLineIdx].size}px Impact`;
     gCtx.textAlign = 'center';
     gCtx.fillText(text, x, y);
     gCtx.strokeText(text, x, y);
+
+
 }
 
 function onTextDraw(text,idx) {
@@ -103,6 +122,8 @@ function addMemeToImg(text){
         gMeme.lines.push({})
     }
     gMeme.lines[gMeme.selectedLineIdx].txt=text
+    gMeme.lines[gMeme.selectedLineIdx].size=20
+
     memeSetLineHeight(initialLineHeight(gMeme.selectedLineIdx))
    
     
@@ -110,18 +131,78 @@ function addMemeToImg(text){
 function initialLineHeight(idx){
     switch(idx){
         case 0: 
-        return 30;
+        return 50;
         case 1:
 return gCanvas.height-30
 case 2:
     return gCanvas.height/2-15
+    case 3:
+    if(gMeme.selectedLineIdx>=2){
+     return (gCanvas.height/2-15)-35
+    }  
+
     }
 }
 
 function memeSetLineHeight(height){
     gMeme.lines[gMeme.selectedLineIdx].y=height
+    gMeme.lines[gMeme.selectedLineIdx].x=canvasCenter
     drawImgFromlocal( gMeme.selectedImgId)
 }
+
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container');
+    gCanvas.width = elContainer.offsetWidth;
+    gCanvas.height = elContainer.offsetHeight;
+}
+
+function renderCategories(){
+    let category;
+    const elContainer = document.querySelector('.categories-container');
+    console.log(elContainer)
+    elContainer.innerHTML=''
+for(category in gKeywords){
+    elContainer.innerHTML+= `<span style="font-size: ${gKeywords[category]*10}px; margin-right:10px" >${category} </span> `
+}
+
+}
+
+//filter category from search box
+function searchOption(searchStr){
+
+    increaseCategoryPressNumber(searchStr)
+    let filterArray = gImgs.filter((img)=>{
+        let res= true;
+          img.keywords.map((keywodrd)=>{
+                if(keywodrd.includes(searchStr)){
+                    console.log(keywodrd,searchStr)
+                }else{
+                    res=false
+                }
+            })
+            console.log(res)
+            return res
+        })
+       filteredImgs= filterArray
+       renderGallery()
+}
+
+function increaseCategoryPressNumber(searchStr){
+    let category;
+    let increase;
+    for(category in gKeywords){
+        if(category===searchStr){
+            gKeywords[category]++;
+        }
+    }
+    renderGallery()
+}
+
+
+
+
+
 
 
 
